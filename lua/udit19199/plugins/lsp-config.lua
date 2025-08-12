618880668e38dafb85fcbf56016497a7331a7875
Udit Agarwal <udit19199@icloud.com>
2025-08-12 08:22:46 +0530

    Updated lsp with ts_ls

--- lua/udit19199/plugins/lsp-config.lua
+++ lua/udit19199/plugins/lsp-config.lua
@@ -3,14 +3,46 @@ return {
     "williamboman/mason.nvim",
     lazy = false,
     config = function()
-      require("mason").setup()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true,
+      ensure_installed = {
+        -- JavaScript/TypeScript ecosystem
+        "ts_ls",              -- TypeScript/JavaScript (updated from tsserver)
+        "tailwindcss",        -- Tailwind CSS
+        "eslint",             -- ESLint linting
+        
+        -- Web technologies
+        "html",               -- HTML
+        "cssls",              -- CSS
+        "emmet_ls",           -- HTML/CSS snippets
+        
+        -- Data & APIs
+        "jsonls",             -- JSON
+        "graphql",            -- GraphQL
+        "prismals",           -- Prisma ORM
+        
+        -- Systems & DevOps
+        "clangd",             -- C/C++
+        "dockerls",           -- Docker
+        "bashls",             -- Bash scripting
+        
+        -- Config
+        "lua_ls",             -- Lua (Neovim config)
+      },
+      automatic_installation = true,
     },
   },
   {
@@ -18,25 +50,153 @@ return {
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+
+      -- TypeScript/JavaScript with Next.js support
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = 'all',
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+            }
+          }
+        },
+        -- Next.js specific file types
+        filetypes = { 
+          "javascript", "javascriptreact", "typescript", "typescriptreact",
+          -- Next.js specific
+          "typescript.tsx", "javascript.jsx"
+        }
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      -- Tailwind CSS
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+
+      -- HTML with better React support
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "templ" },
+      })
+
+      -- CSS
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+
+      -- JSON
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
+
+      -- ESLint
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+        settings = {
+          workingDirectory = { mode = "auto" }
+        }
+      })
+
+      -- Prisma ORM
+      lspconfig.prismals.setup({
+        capabilities = capabilities,
+      })
+
+      -- C/C++ development
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        cmd = { "clangd", "--background-index", "--clang-tidy", "--header-insertion=iwyu" },
+        filetypes = { "c", "cpp", "cc", "cxx", "h", "hpp" },
+      })
+
+      -- Emmet for fast HTML/CSS (supports JSX/TSX)
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { 
+          "html", "css", "scss", "sass", "less",
+          -- React/Next.js support
+          "typescriptreact", "javascriptreact", "jsx", "tsx"
+        }
+      })
+
+      -- GraphQL
+      lspconfig.graphql.setup({
+        capabilities = capabilities,
+        filetypes = { "graphql", "gql", "svelte", "astro", "vue", "typescript", "javascript" }
+      })
+
+      -- Docker
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
+
+      -- Lua (for Neovim config)
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT"
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+
+      -- Global LSP handlers for better UX
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+      })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+      })
+
+      -- Diagnostic configuration
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "●", -- Could be '■', '▎', 'x'
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+
+      -- Diagnostic keymaps
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+      vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Diagnostic list" })
     end,
+  },
+  -- Schema store for JSON
+  {
+    "b0o/schemastore.nvim",
   },
 }
--- lua/udit19199/remap.lua
+++ lua/udit19199/remap.lua
@@ -1,20 +1,89 @@
--- ~/.config/nvim/lua/udit19199/mappings.lua
-
--- Set leader key to space
+-- Enhanced keymaps for MERN stack development
 vim.g.mapleader = " "
 
--- Basic mappings
-
-vim.keymap.set("n", "<leader>w", "<C-w>h", {noremap = true, silent = true})
-
--- Save file
-vim.keymap.set("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
-
--- Close buffer
+-- ======================= FILE OPERATIONS =======================
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>W", "<cmd>wa<CR>", { desc = "Save all files" })
 vim.keymap.set("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa<CR>", { desc = "Quit all" })
+
+-- ======================= NAVIGATION =======================
+-- Quick window navigation (better than your current <leader>w)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Buffer navigation
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+
+-- ======================= EDITING =======================
+-- Keep your existing mappings but improved
+vim.keymap.set("n", "<leader>a", "ggVG", { desc = "Select all" })
+vim.keymap.set("n", "<leader>z", "u", { desc = "Undo" })
+
+-- Better search and replace (enhanced version of your C-a mapping)
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>//g<Left><Left>]], { desc = "Search and replace word under cursor" })
+vim.keymap.set("v", "<leader>sr", [[:s/\V<C-R>=escape(@",'/\')<CR>//g<Left><Left>]], { desc = "Search and replace selection" })
 
 -- Clear search highlights
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>a", "ggVG", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>z", "u", { noremap = true, silent = true })
 
+-- ======================= CODE ACTIONS =======================
+-- LSP mappings (will be used in lsp-config.lua)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+
+-- Formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format file" })
+
+-- ======================= TELESCOPE MAPPINGS =======================
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Help tags" })
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope commands<CR>", { desc = "Commands" })
+
+-- ======================= PROJECT SPECIFIC =======================
+-- Package.json script runner
+vim.keymap.set("n", "<leader>ps", "<cmd>Telescope npm scripts<CR>", { desc = "NPM scripts" })
+
+-- Git mappings
+vim.keymap.set("n", "<leader>gg", "<cmd>Telescope git_status<CR>", { desc = "Git status" })
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", { desc = "Git commits" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", { desc = "Git branches" })
+
+-- ======================= QUICK ACTIONS =======================
+-- Split windows
+vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>", { desc = "Horizontal split" })
+
+-- Terminal
+vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR>", { desc = "Open terminal" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- ======================= HACKATHON SPEED MAPPINGS =======================
+-- Quick file creation
+vim.keymap.set("n", "<leader>nf", "<cmd>enew<CR>", { desc = "New file" })
+
+-- Copy file path
+vim.keymap.set("n", "<leader>cp", "<cmd>let @+ = expand('%:p')<CR>", { desc = "Copy file path" })
+
+-- Toggle word wrap
+vim.keymap.set("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
+
+-- ======================= VISUAL MODE ENHANCEMENTS =======================
+-- Move lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Better indenting
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
